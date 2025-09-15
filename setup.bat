b412f2df74767bff41ebea401e905b766edb7919
gluten-h <jonza1011@gmail.com>
2025-09-15 23:36:15 +0400

    vcpkg fix

--- setup.bat
+++ setup.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 
-git submodule update
+git submodule update --init --recursive
 if not exist vcpkg\vcpkg.exe call vcpkg\bootstrap-vcpkg.bat
 call vcpkg\vcpkg.exe install directxtex:x64-windows
 call vcpkg\vcpkg.exe install directxtk:x64-windows
@@ -8,3 +8,5 @@ call vcpkg\vcpkg.exe integrate install
 
 if not exist build\ mkdir build\
 call cmake -B build\
+
+pause
